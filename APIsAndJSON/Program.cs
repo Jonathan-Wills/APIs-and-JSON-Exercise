ee80bef21933a16bd1e7022cbbe40b248480acdc
Jonathan-Wills <wills830@hotmail.com>
2022-11-29 16:45:56 -0500

    initial commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+            var quote = new RonVSKanyeAPI(client);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: '{quote.KanyeQuote()}'");
+                Console.WriteLine($"---------------------");
+                Console.WriteLine($"Ron: '{quote.RonQuote()}'");
+                Console.WriteLine($"---------------------");
+            }
+
+
+            Console.WriteLine($"Please enter your API Key");
+            var api_key = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Please enter the name of the city");
+                var city_name = Console.ReadLine();
+                var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}";
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+                Console.WriteLine($"Would you like to choose a different city?");
+                var userinput = Console.ReadLine();
+                Console.WriteLine();
+                if(userinput.ToLower() == "no")
+                {
+                    break;
+                }
+            }
         }
+        
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        private readonly HttpClient _client;
+        public RonVSKanyeAPI(HttpClient client)
+        {
+            _client = client;
+        }
+        public string KanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonQuote()
+        {            
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+        }
     }
 }
